092f7b748769bb433cfd2672cb07094f58e9846f
kiwi-choe <magmag0221@gmail.com>
2024-07-14 12:19:18 +0900

    firebase setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:motd/screens/home_screen.dart';
 import 'package:motd/service/photo_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   PhotoService().getPhotos();
   runApp(const App());
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class App extends StatelessWidget {
